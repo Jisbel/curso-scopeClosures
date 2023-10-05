817528157690d8d5ff419fd456d8b6ea9ae4b08d
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-05 17:05:24 -0400

    learning re asing and re declare

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName ; // undefined
+firstName = 'Jisbel';
+console.log(firstName);
+
+var lastName = 'Carolina'; //declara / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'Carolina'; // declarando / asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// let
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+// let fruit = 'Banana'; // fruit ya esta declarada
+// console.log(fruit);
+
+// const
+const animal = 'Dog'; //declarar y asignar
+// animal = 'Cat'; // reasignar  (no se puede reasignar una const)
+// const animal = 'Snake'; // no se puede reasignar
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
